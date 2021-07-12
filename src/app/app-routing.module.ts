706e2ef18e62f9a29ab36e599d345e6f77edac6b
Stefan Knaak <stefan@knaak.org>
2021-07-12 12:12:53 +0200

    bump

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     HomeRoutingModule,
     DetailRoutingModule
   ],
--- src/app/floatingweb/floatingweb.component.spec.ts
+++ src/app/floatingweb/floatingweb.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FloatingwebComponent } from './floatingweb.component';
 
@@ -6,7 +6,7 @@ describe('FloatingwebComponent', () => {
   let component: FloatingwebComponent;
   let fixture: ComponentFixture<FloatingwebComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FloatingwebComponent ]
     })
